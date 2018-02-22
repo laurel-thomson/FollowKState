a2db6c0b6112603a9c774196208932177a35e0f8
Laurel <laurel.beth.thomson@gmail.com>
2018-02-22 16:54:25 -0600

    Combined Luke, Alex, and Jeremy work from first session.  (Tweet class and tweet_list_item layout)

--- app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.jeremy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Tweet  Danktweet = new Tweet("Duke Dangston", "Is the best", null, 270000, 0, 77,"@yaboi", null, null, null, false);
+
+        ListView Whateveryouwant = (ListView) findViewById(R.id.listview);
     }
 }
+
---
+++ app/src/main/java/com/example/jeremy/myapplication/Tweet.java
@@ -0,0 +1,42 @@
+package com.example.jeremy.myapplication;
+
+import java.util.Date;
+
+public class Tweet {
+
+    public String mAuthor;
+    public String mText;
+    public Date mDate;
+    public int mLikes;
+    public int mRetweets;
+    public int mReplies;
+    public String mHandle;
+    public String mImageUrl;
+    public String mVideoUrl;
+    public boolean mIsRetweet;
+    public String mProfilePictureUrl;
+
+    public Tweet(String author,
+                 String text,
+                 Date date,
+                 int likes,
+                 int retweets,
+                 int replies,
+                 String handle,
+                 String imageUrl,
+                 String profilePictureUrl,
+                 String videoUrl,
+                 boolean isRetweet) {
+        this.mAuthor = author;
+        this.mText = text;
+        this.mDate = date;
+        this.mLikes = likes;
+        this.mHandle = handle;
+        this.mRetweets = retweets;
+        this.mIsRetweet = isRetweet;
+        this.mReplies = replies;
+        this.mImageUrl = imageUrl;
+        this.mProfilePictureUrl = profilePictureUrl;
+        this.mVideoUrl = videoUrl;
+    }
+}
