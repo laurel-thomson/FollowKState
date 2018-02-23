da34dba78de6c52be9e9db958d1499089c9a8297
Laurel <laurel.beth.thomson@gmail.com>
2018-02-23 07:52:05 -0600

    Added some friendly comments!

--- app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
@@ -13,16 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this is where we made some Tweets: make some more and trying changing the other parameters!
         Tweet  Danktweet = new Tweet("Duke Dangston", "Is the best", null, 270000, 0, 77,"@yaboi", null, null, null, false);
         Tweet fakeNews = new Tweet("CNN", "luke", null, 0, 0, 0, null, null, null, null, false);
+
+        //this is where we bundled up the Tweets into a nice list
         ArrayList<Tweet> list = new ArrayList<Tweet>();
+
+        //this is where we added the Tweets to the list
         list.add(Danktweet);
         list.add(fakeNews);
 
+        //this is where we make an instance of TweetAdapter named "tweetyBird".  We give is the list
+        //of Tweets.
         TweetAdapter tweetyBird = new TweetAdapter(this, 0, list);
 
+        //Find the ListView that we made in the activity_main layout file and give it a name
         ListView Whateveryouwant = (ListView) findViewById(R.id.listview);
 
+        //Connect the ListView with the TweetAdapter
         Whateveryouwant.setAdapter(tweetyBird);
     }
 }
--- app/src/main/java/com/example/jeremy/myapplication/TweetAdapter.java
+++ app/src/main/java/com/example/jeremy/myapplication/TweetAdapter.java
@@ -13,43 +13,41 @@ import android.widget.TextView;
 
 import java.util.List;
 
-
-/**
- * Created by brummerjd on 2/22/18.
- */
     public class TweetAdapter extends ArrayAdapter<Tweet>
 {
-
+    //This is the Constructor.  We called it in MainActivity when we made "tweetyBird" - the adapter.
+    //The third parameter is a list of Tweets...which is what we gave it to do its job!
     public TweetAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Tweet> objects) {
         super(context, resource, objects);
     }
 
+    //This is a method(function) from the ArrayAdapter class that we are overriding with
+    //our own stuff.  It inflates each individual Tweet list item view.
     @Override
-
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        // Get the data item for this position
-
+        //It loops through the list of Tweets behind the scenes and gets the Tweet from each position.
         Tweet tweet = getItem(position);
 
         // Check if an existing view is being reused, otherwise inflate the view
-
         if (convertView == null) {
-
+            //Here is where we connect the TweetAdapter to the tweet_list_item layout that Jeremy made
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.tweet_list_item, parent, false);
 
         }
 
-        // Lookup view for data population
-
+        //Here is where we found one of the individual layout items in tweet_list_item.
+        //You can add more references here and set more of the fields with data from
+        //the Tweets!
         TextView tweetContent = (TextView) convertView.findViewById(R.id.tweet_content);
 
-        // Populate the data into the template view using the data object
-
+        //Here is where we gave the TextView the text from the Tweet that will show
+        //up in the TextView. If you added more layout items above, you'll need to set
+        //their text here.
+        //Don't worry about pictures yet...that will be more complicated.
         tweetContent.setText(tweet.mText);
 
         // Return the completed view to render on screen
-
         return convertView;
 
     }
