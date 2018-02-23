6d0d6f252149262516d8213f45fbfed2bde7c9ee
brummerjd <jbrummer1335@gmail.com>
2018-02-22 20:12:42 -0600

    made a damn sweet app

--- app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,8 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Tweet  Danktweet = new Tweet("Duke Dangston", "Is the best", null, 270000, 0, 77,"@yaboi", null, null, null, false);
+        Tweet fakeNews = new Tweet("CNN", "luke", null, 0, 0, 0, null, null, null, null, false);
+        ArrayList<Tweet> list = new ArrayList<Tweet>();
+        list.add(Danktweet);
+        list.add(fakeNews);
+
+        TweetAdapter tweetyBird = new TweetAdapter(this, 0, list);
 
         ListView Whateveryouwant = (ListView) findViewById(R.id.listview);
+
+        Whateveryouwant.setAdapter(tweetyBird);
     }
 }
 
---
+++ app/src/main/java/com/example/jeremy/myapplication/TweetAdapter.java
@@ -0,0 +1,56 @@
+package com.example.jeremy.myapplication;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+/**
+ * Created by brummerjd on 2/22/18.
+ */
+    public class TweetAdapter extends ArrayAdapter<Tweet>
+{
+
+    public TweetAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Tweet> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Get the data item for this position
+
+        Tweet tweet = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.tweet_list_item, parent, false);
+
+        }
+
+        // Lookup view for data population
+
+        TextView tweetContent = (TextView) convertView.findViewById(R.id.tweet_content);
+
+        // Populate the data into the template view using the data object
+
+        tweetContent.setText(tweet.mText);
+
+        // Return the completed view to render on screen
+
+        return convertView;
+
+    }
+}
