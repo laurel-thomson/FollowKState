1cf32c40715fbec8a6c8fd941a62aca63de0102a
Tiatan95 <jjohn411@ksu.edu>
2018-03-15 20:32:49 -0500

    Made filterTweet method

--- app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jeremy.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case CODE_TWEETS_OBTAINED:
-                TweetAdapter tweetyBird = new TweetAdapter(this, 0, mTweets);
+                TweetAdapter tweetyBird = new TweetAdapter(this, 0, filterTweets());
                 ListView Whateveryouwant = (ListView) findViewById(R.id.listview);
                 Whateveryouwant.setAdapter(tweetyBird);
         }
@@ -60,6 +62,17 @@ public class MainActivity extends AppCompatActivity {
             // Use ActivityResult to notify main thread that tweets have been obtains (shouldn't update UI in ASync task)
             onActivityResult(CODE_TWEETS_OBTAINED, -1, null);
         }
+    }
+
+    private ArrayList<Tweet> filterTweets(){
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        ArrayList<Tweet> Easy = new ArrayList<Tweet>();
+        for (int i =0;i<mTweets.size();i=i+1) {
+            if (pref.getBoolean(mTweets.get(i).mHandle, true)){
+Easy.add(mTweets.get(i));
+
+            }
+        }return Easy;
     }
 }
 
